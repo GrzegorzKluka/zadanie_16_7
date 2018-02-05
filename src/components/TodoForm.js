0b18601ad0ead65452535d53169ee1c1e1c1a295
Grzegorz Kluka <g.kluka@wp.pl>
2018-02-05 18:08:07 +0100

    Fixed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,9 +4,9 @@ import style from './TodoForm.css';
 class TodoForm extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: '' }; // ??
+		this.state = { value: '' };
 
-		this.handleChange = this.handleChange.bind(this); // ??
+		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 

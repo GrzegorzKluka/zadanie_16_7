bbb68596aa74dd1206f2b8e66e5510337d08213b
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-02-05 23:20:19 +0100

    provide HMR implementation

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app'),
+  )
+}
+
+render(App)
 
 if (module.hot) {
 	module.hot.accept('./containers/App', () => {
-		const NextApp = require('./containers/App').default;
-		ReactDOM.render(
-			<AppContainer>
-				<NextApp />
-			</AppContainer>,
-			document.getElementById('app'),
-		);
+    render(App)
 	});
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,73 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeJsPlugin = require('optimize-js-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OptimizeJsPlugin = require('optimize-js-plugin')
 
-module.exports = {
-	entry: ['react-hot-loader/patch', './src/index.js'],
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'app.bundle.js',
-	},
+const env = process.env.NODE_ENV || 'development'
 
-	devServer: {
-		contentBase: path.join(__dirname, 'build'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: 'style-loader' },
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-						},
-					},
-				],
-			},
-		],
-	},
-};
-
-var env = process.env.NODE_ENV || 'development';
-
-console.log('NODE_ENV:', env);
-
-if (env === 'production') {
-	plugins.push(
-		new webpack.optimize.UglifyJsPlugin(),
-		new OptimizeJsPlugin({
-			sourceMap: false,
-		}),
-	);
+const base = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+    publicPath: '/',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
+
+const production = () => {
+  return Object.assign({}, base, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin(),
+      new OptimizeJsPlugin({
+        sourceMap: false,
+      }),
+      ...base.plugins,
+    ],
+  })
+}
+const development = () => {
+  return Object.assign({}, base, {
+    entry: ['react-hot-loader/patch', ...base.entry],
+    devServer: {
+      contentBase: path.join(__dirname, 'build'),
+      hot: true,
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      ...base.plugins,
+    ],
+  })
+}
+
+module.exports = () => {
+  if (env === 'production') {
+    return production()
+  } else {
+    return development()
+  }
+}
+
